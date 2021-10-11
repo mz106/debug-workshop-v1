126543830377acef68ebbecc524c80990c0e55f0
mz106 <flashman123@fastmail.fm>
2021-10-11 20:05:56 +0100

    three bugs

--- data.js
+++ data.js
@@ -1,7 +1,7 @@
 
 //contains two arrays - one the list of employees and the other the list of employees recieving bonuses
 
-const employees = ['sandra jones', 'chris lundy', 'andy smith', 'lucy davis', 'ben pleasance', 'sophie darling', 'jill white', 'fred greene', 'james black'];
+const employee = ['sandra jones', 'chris lundy', 'andy smith', 'lucy davis', 'ben pleasance', 'sophie darling', 'jill white', 'fred greene', 'james black'];
 const bonus = ['sandra jones', 'ben pleasance', 'fred greene']
 
 module.exports = {
